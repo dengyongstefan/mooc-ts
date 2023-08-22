d835dba9a04f8240cb08d72978a32559aaaa4a71
Deng Yong <1060756423@qq.com>
2023-08-22 16:53:12 +0800

    update

--- chapter2/infer.ts
+++ chapter2/infer.ts
@@ -3,3 +3,14 @@ type paramsType<T> = T extends (arg: infer P) => any ? P : T
 type Func = (user:number) => number
 
 type A = paramsType<Func>
+
+interface Customer {
+    name:string
+    degree:string
+}
+
+type AddAttrToObj<T,K extends string, V> = {
+    [P in keyof T | K] :P extends keyof T ? T[P] : V
+}
+
+type B = AddAttrToObj<Customer,'weixin',number>
